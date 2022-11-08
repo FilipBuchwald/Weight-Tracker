08679d9f91048e39b5b48aeb556802fba1776c2f
Filip Buchwald <fixu1998@gmail.com>
2022-11-08 12:40:11 +0100

    added onChange to input

--- src/Form/index.js
+++ src/Form/index.js
@@ -4,27 +4,41 @@ import DatePicker from "react-datepicker";
 import { useState } from 'react';
 
 const Form = () => {
-  
+
   const onFormSubmit = (event) => {
     event.preventDefault();
+    console.log(` Twoja waga w dniu ${startDate.toLocaleDateString()} wynosi ${weight} kg`);
   };
 
   const [startDate, setStartDate] = useState(new Date());
+  const [weight, setWeight] = useState("");
+
+  const onInputChange = ({target}) => setWeight(target.value);
 
   return (
 
     <StyledForm onSubmit={onFormSubmit}>
       <Title>Data</Title>
-      <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+      <DatePicker 
+      selected={startDate} 
+      onChange={(date) => setStartDate(date)} 
+      />
       <Title>Waga</Title>
-      <Input type="number" min="0" step="0.1"></Input>
-      
+      <Input
+        type="number"
+        min="0"
+        step="0.1"
+        value={weight}
+        onChange = {onInputChange}
+        placeholder="Wpisz swoją aktualną wagę"
+      ></Input>
+
       <Button> Dodaj Pomiar </Button>
     </StyledForm>
 
   );
 };
 
-//placeholderText="Click to select a date"
+
 
 export default Form;
