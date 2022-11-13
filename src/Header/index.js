b40546f52955707712f05ee0512af3be6dec0ff8
Filip Buchwald <fixu1998@gmail.com>
2022-11-13 16:25:48 +0100

    added header component

---
+++ src/Header/index.js
@@ -0,0 +1,8 @@
+import { StyledH1, StyledH2 } from "./styled";
+
+export const Header = () => (
+  <>
+  <StyledH1>Dodaj pomiar</StyledH1>
+  <StyledH2>Wprowadz dane aby przeanalizować wyniki</StyledH2>
+  </>
+);
---
+++ src/Header/styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledH1 = styled.h1`
+margin: 10px 30px;
+font-size: 24px;
+`;
+
+export const StyledH2 = styled.h2`
+margin: 10px 30px;
+font-size: 14px;
+font-weight: 500;
+color: grey;
+`;
