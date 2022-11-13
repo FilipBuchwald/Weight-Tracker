d0dfe80f9bcf36217606180c86d24172a49131a3
Filip Buchwald <fixu1998@gmail.com>
2022-11-13 16:25:14 +0100

    added save result interaction

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,44 +1,71 @@
-import { Button, Input, StyledForm, Title } from "./styled";
-import "react-datepicker/dist/react-datepicker.css";
-import DatePicker from "react-datepicker";
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Button, Input, StyledForm, Title, List, Item, Content, Wrapper } from "./styled";
+import { Container } from '../Container/styled';
+import { Header } from '../Header';
 
 const Form = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [weight, setWeight] = useState("");
+  const [result, setResult] = useState([]);
 
   const onFormSubmit = (event) => {
     event.preventDefault();
-    console.log(` Twoja waga w dniu ${startDate.toLocaleDateString()} wynosi ${weight} kg`);
+    saveResult(startDate, weight);
+    setWeight("");
   };
 
-  const [startDate, setStartDate] = useState(new Date());
-  const [weight, setWeight] = useState("");
+  const saveResult = (startDate, weight) => {
+    setResult(result => [
+      ...result,
+      {
+        date: startDate,
+        weight: weight,
+        id: uuidv4(),
+      }
+    ])
+  };
 
-  const onInputChange = ({target}) => setWeight(target.value);
+  const onInputChange = ({ target }) => setWeight(target.value);
 
   return (
-
-    <StyledForm onSubmit={onFormSubmit}>
-      <Title>Data</Title>
-      <DatePicker 
-      selected={startDate} 
-      onChange={(date) => setStartDate(date)} 
-      />
-      <Title>Waga</Title>
-      <Input
-        type="number"
-        min="0"
-        step="0.1"
-        value={weight}
-        onChange = {onInputChange}
-        placeholder="Wpisz swoją aktualną wagę"
-      ></Input>
-
-      <Button> Dodaj Pomiar </Button>
-    </StyledForm>
-
+    <Container>
+      <Header />
+      <StyledForm
+        onSubmit={onFormSubmit}
+      >
+        <Wrapper>
+          <Title>data</Title>
+          <DatePicker
+            className="data"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+          />
+          <Title>waga</Title>
+          <Input
+            type="number"
+            min="0"
+            step="0.1"
+            value={weight}
+            onChange={onInputChange}
+            placeholder="Wpisz swoją aktualną wagę"
+          />
+        </Wrapper>
+        <Button> Dodaj Pomiar </Button>
+        <List>
+          {result.map(({ id, date, weight }) => (
+            <Item key={id}>
+              <Content>
+                Twoja waga w dniu {date.toLocaleDateString()} wyniosła {weight} kg
+              </Content>
+            </Item>
+          ))}
+        </List>
+      </StyledForm>
+    </Container>
   );
 };
-
-
 
 export default Form;
--- src/Form/styled.js
+++ src/Form/styled.js
@@ -1,21 +1,50 @@
 import styled from "styled-components";
 
 export const StyledForm = styled.form`
-display: grid;
-grid-template-columns: 1fr;
-
+  display: grid;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  align-items: center; 
 `;
 
 export const Input = styled.input`
+  width: 180px;
+  height: 34px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
   margin: 10px;
 `;
 
 export const Button = styled.button`
   margin: 10px;
+  width: 120px;
+  padding: 10px;
+  border-radius: 5px;
+  border: none;
+  color: white;
+  background-color: #d9ba32;
 `;
 
-export const Title = styled.h2`
-  margin: 10px;
-  font-weight: 600;
-  font-size: 18px;
+export const Title = styled.h3`
+  margin: 0px 10px;
+  font-weight: 700;
+  font-size: 12px;
+`;
+
+export const List = styled.ul`
+  list-style-type: none;
+`;
+
+export const Item = styled.li`
+  border-bottom: 1px solid #ddd;
+`;
+
+export const Content = styled.span`
+  font-size: 14px;
+`;
+
+export const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  border-bottom: 1px solid #ddd;
 `;
